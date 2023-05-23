1e33a7bd30672aef051e4bf1429f334afc86590d
ilker ciliv <ikesan@ilkers-mbp.myfiosgateway.com>
2023-05-22 23:30:33 -0400

    templates and static files added

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -51,4 +50,4 @@ def monthly_challenge(request, month):
             "text": challenge_text,
         })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        raise Http404()
